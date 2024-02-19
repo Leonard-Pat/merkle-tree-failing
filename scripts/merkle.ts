0952a864f12a17557ab4d2431af5e91ca47451f8
Leonard Paturel <leonardpaturel@gmail.com>
2024-02-19 11:11:26 +0000

    updated for pedersen

--- scripts/merkle.ts
+++ scripts/merkle.ts
@@ -5,21 +5,42 @@ const raw = [
   [0x1, 0x3],
 ];
 
-const leaves = raw.map((leaf) => hash.computePoseidonHashOnElements(leaf));
+function poseidonMerkleTree() {
+  const leaves = raw.map((leaf) => hash.computePoseidonHashOnElements(leaf));
 
-const merkleTree = new merkle.MerkleTree(leaves, hash.computePoseidonHash);
-const merkleRoot = merkleTree.root;
-const proofLeaf0 = merkleTree.getProof(leaves[0]);
+  const merkleTree = new merkle.MerkleTree(leaves, hash.computePoseidonHash);
+  const merkleRoot = merkleTree.root;
+  const proofLeaf0 = merkleTree.getProof(leaves[0]);
 
-console.log("merkle root", merkleRoot);
-console.log("Leaf 1", leaves[0]);
-console.log("proof", proofLeaf0);
-console.log(
-  "Verify",
-  merkle.proofMerklePath(
-    merkleRoot,
-    leaves[0],
-    proofLeaf0,
-    hash.computePoseidonHash
-  )
-);
+  console.log("Poseidon Merkle Root - POSEIDON", merkleRoot);
+  console.log("Leaf 0 - POSEIDON", leaves[0]);
+  console.log("Proof 0 - POSEIDON", proofLeaf0);
+  console.log(
+    "Verify",
+    merkle.proofMerklePath(
+      merkleRoot,
+      leaves[0],
+      proofLeaf0,
+      hash.computePoseidonHash
+    )
+  );
+}
+
+function pedersenMerkleTree() {
+  const leaves = raw.map((leaf) => hash.computePedersenHashOnElements(leaf));
+
+  const merkleTree = new merkle.MerkleTree(leaves);
+  const merkleRoot = merkleTree.root;
+  const proofLeaf0 = merkleTree.getProof(leaves[0], merkleTree.leaves);
+
+  console.log("Poseidon Merkle Root - PEDERSEN", merkleRoot);
+  console.log("Leaf 0 - PEDERSEN", leaves[0]);
+  console.log("Proof 0 - PEDERSEN", proofLeaf0);
+  console.log(
+    "Verify",
+    merkle.proofMerklePath(merkleRoot, leaves[0], proofLeaf0)
+  );
+}
+
+poseidonMerkleTree();
+pedersenMerkleTree();
